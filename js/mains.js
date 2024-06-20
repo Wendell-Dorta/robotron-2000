51b52224083ada152251db53364fc446b37947a6
LESTE 3 - FADLO HAIDAR <e044337w10@professor.educacao.sp.gov.br>
2024-06-19 21:05:06 -0300

    fiz as aulas 1 e 2

---
+++ js/mains.js
@@ -0,0 +1,12 @@
+const robotron = document.querySelector("#robotron");
+
+robotron.addEventListener("click", function () {
+  console.log("Clique no robo");
+});
+
+function dizOi(nome) {
+  console.log("Oi" = nome);
+  console.log("Bem-Vindo ao Robotron-2000");
+}
+
+dizOi("Wendell");
